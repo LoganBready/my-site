475a57cc22d7a738b84b23018e52c3dd18138090
Logan Bready <logan.bready@gmail.com>
2022-09-13 22:53:27 -0500

    homepage update

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -1,12 +1,13 @@
 //styles
 import styles from './HomeContent.module.scss';
-import { TypeAnimation } from 'react-type-animation';
+import {FaAngleDoubleRight} from 'react-icons/fa';
 
 export const HomeContent = () => {
     return (
         <>
-        <h1 className={styles.H1}>Hello, I'm <span className={styles.Span}>Logan</span>.</h1>
-        <h2 className={styles.H2}>I'm a <span className={styles.Typed}>front-end engineer</span>.</h2>
+          <h1 className={styles.H1}>Hello, I'm <span className={styles.Span}>Logan</span>.</h1>
+          <h2 className={styles.H2}>I'm a <span className={styles.Typed}>front-end engineer</span>.</h2>
+          <a className={styles.ContactBtn}>Contact <FaAngleDoubleRight fontSize={'20px'}/></a>
         </>
     )
 }
