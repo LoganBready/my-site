191ed90ebf1db198b7046c8a40e68d046693fc69
logan <logan.bready@gmail.com>
2023-08-27 11:40:58 -0500

    spacing fix

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 import eightBitLogan from '../../../public/images/8-bit-logan.png'
 import clearLogan from '../../../public/images/clear-logan.png'
 import styles from './About.module.scss'
-import { Links } from '../Links'
-import { linksData } from '../Links/links.constants'
 import { useState } from 'react'
 
 export const About = () => {
