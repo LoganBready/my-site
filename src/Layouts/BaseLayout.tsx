2ca953032958254634ce2dadbeb3566eb55ac22e
Logan Bready <logan.bready@gmail.com>
2023-09-02 19:20:47 -0500

    add google

--- src/Layouts/BaseLayout.tsx
+++ src/Layouts/BaseLayout.tsx
@@ -1,12 +1,9 @@
 import Head from 'next/head'
 import { Navigation } from '../components/Navigation'
-import Script from 'next/script'
 
 interface IBaseLayout {
   children?: React.ReactNode
 }
-
-const googleTag = process.env.GOOGLE_ANALITICS
 
 export const BaseLayout = ({ children }: IBaseLayout) => {
   return (
@@ -25,18 +22,6 @@ export const BaseLayout = ({ children }: IBaseLayout) => {
             href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
           />
         </Head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${googleTag}`}
-        />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${googleTag}');
-        `}
-        </Script>
         <Navigation />
         <main id="main-content" data-content="main">
           {children}
