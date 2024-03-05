611538cfe4e10a2fec3a0ecccc8334bce0129c49
logan <logan.bready@gmail.com>
2024-03-05 15:44:40 -0600

    work

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,20 +1,12 @@
 import Image from 'next/image'
-import eightBitLogan from '../../../public/images/8-bit-logan.png'
 import clearLogan from '../../../public/images/clear-logan.png'
 import styles from './About.module.scss'
-import { useState } from 'react'
 
 export const About = () => {
-  const [imageSrc, setImageSrc] = useState(eightBitLogan)
-
   return (
     <div className={styles.aboutContainer}>
-      <div
-        className={styles.headShotContainer}
-        onMouseOver={() => setImageSrc(clearLogan)}
-        onMouseOut={() => setImageSrc(eightBitLogan)}
-      >
-        <Image src={imageSrc} alt="8-bit-logan" height="250" width="250" />
+      <div className={styles.headShotContainer}>
+        <Image src={clearLogan} alt="8-bit-logan" height="250" width="250" />
       </div>
       <div className={styles.aboutInformation}>
         <h1 className={styles.heading}>Logan Bready</h1>
